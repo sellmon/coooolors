dca20835581ed477d2be928ffd944a599d5797fe
sellmon <108669343+sellmon@users.noreply.github.com>
2022-08-17 12:36:39 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,15 @@
+const colors = ["lime", "orange", "rgba(111,0,120)", "black", "#f15025"];
+const btn = document.getElementById('btn');
+const color = document.querySelector('.color');
+
+btn.addEventListener('click',() => {
+    //get random number between 0-3 colors [3]
+    const randomNumber = getRandomNumber();
+    console.log(randomNumber)
+    document.body.style.backgroundColor = colors[randomNumber]
+    color.textContent = colors[randomNumber]
+});
+
+function getRandomNumber() {
+    return Math.floor(Math.random()*colors.length);
+};
---
+++ hex.js
@@ -0,0 +1,20 @@
+const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "a", "b", "c", "d", "e", "f"];
+const btn = document.getElementById('btn')
+const color = document.querySelector('.color')
+
+btn.addEventListener('click', () => {
+    let hexColor = '#';
+    // for(let i=0; i<6; i++) {
+    //     hexColor += hex [getRandomNumber()]
+    // }
+    Object.values(hex).forEach(values => {
+        if (values%1.5)
+        hexColor += hex [getRandomNumber()]
+    })
+    color.textContent = hexColor;
+    document.body.style.backgroundColor = hexColor;
+})
+
+function getRandomNumber() {
+    return Math.floor (Math.random() * hex.length)
+}
